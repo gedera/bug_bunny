88ed95a61ca9ec25c8aa98bf9ad82c3e53eb700e
Gabriel <gab.edera@gmail.com>
2026-02-16 19:08:06 -0300

    lib/bug_bunny/middleware/stack.rb clean with rubocop

--- lib/bug_bunny/middleware/stack.rb
+++ lib/bug_bunny/middleware/stack.rb
@@ -1,47 +1,31 @@
 # frozen_string_literal: true
 
-# lib/bug_bunny/middleware/stack.rb
-
 module BugBunny
   module Middleware
-    # Gestiona una pila (stack) de middlewares para procesar peticiones y respuestas.
+    # Gestiona una pila de middlewares (Chain of Responsibility).
     #
-    # Implementa el patrón "Builder" para construir una cadena de responsabilidades.
-    # Permite registrar clases de middleware que envolverán la ejecución final (el Producer).
-    # Es similar en funcionamiento a `Rack::Builder` o `Faraday::RackBuilder`.
-    #
-    # @example Construcción manual
-    #   stack = BugBunny::Middleware::Stack.new
-    #   stack.use BugBunny::Middleware::Logger
-    #   stack.use BugBunny::Middleware::JsonResponse
-    #
-    #   # 'app' será el Logger, que llama a JsonResponse, que llama a final_producer
-    #   app = stack.build(final_producer)
-    #   app.call(request)
+    # Permite apilar clases que interceptan y modifican las peticiones/respuestas
+    # antes de llegar al Productor final, similar a Rack o Faraday.
     class Stack
-      # Inicializa una nueva pila de middlewares vacía.
       def initialize
         @middlewares = []
       end
 
       # Registra un middleware en la pila.
       #
-      # @param klass [Class] La clase del middleware. Debe tener un constructor `initialize(app, *args)` y un método `call(env)`.
-      # @param args [Array] Argumentos opcionales que se pasarán al constructor del middleware.
-      # @yield [block] Bloque opcional que se pasará al constructor del middleware.
-      # @return [Array] La lista actualizada de configuraciones de middleware.
+      # @param klass [Class] La clase del middleware.
+      #   Debe tener un constructor `initialize(app, *args)` y un método `call(env)`.
+      # @param args [Array] Argumentos opcionales para el constructor del middleware.
+      # @param block [Proc] Bloque opcional de configuración.
       def use(klass, *args, &block)
         @middlewares << { klass: klass, args: args, block: block }
       end
 
-      # Construye la cadena de ejecución (app) componiendo todos los middlewares registrados.
+      # Construye la cadena de ejecución enlazando todos los middlewares.
       #
-      # Itera sobre los middlewares en orden inverso, envolviendo la `final_app` capa por capa.
-      # Esto asegura que el primer middleware agregado con {#use} sea el más externo y, por tanto,
-      # el primero en recibir la llamada `call`.
-      #
-      # @param final_app [Proc, Object] El objeto final que recibirá la petición (generalmente el Producer). Debe responder a `call`.
-      # @return [Object] El primer eslabón de la cadena (el middleware más externo).
+      # @param final_app [Proc, Object] El objeto final que recibirá la petición (generalmente el Producer).
+      #   Debe responder a `call`.
+      # @return [Object] El primer middleware de la cadena (el punto de entrada).
       def build(final_app)
         @middlewares.reverse.inject(final_app) do |app, config|
           config[:klass].new(app, *config[:args], &config[:block])
